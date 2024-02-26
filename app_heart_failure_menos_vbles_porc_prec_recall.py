f238ae4796b56550ba875d4e963a31dd91e6208f
serbengut <127719253+serbengut@users.noreply.github.com>
2024-02-26 12:24:00 +0100

    Add files via upload

--- app_heart_failure_menos_vbles_porc_prec_recall.py
+++ app_heart_failure_menos_vbles_porc_prec_recall.py
@@ -92,6 +92,7 @@ with colu1:
 with colu2:
     #umbral_usu = st.number_input(label='Definir el Umbral (entre 0 y 1)', min_value=0.01, max_value=0.99,step=0.01,format="%.2f")
     umbral_usu = 0.12
+    umbral_usu
 st.divider()
 
 ########################################################################################
